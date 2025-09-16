d18bfc25af42e238dc1e21db506313cfa7dc7ab3
lua-rib <luana_rsantos@outlook.com>
2025-09-16 12:25:15 -0300

    codando

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const inputTarefa = document.querySelector('.input-itens')
+const botaoTarefa = document.querySelector('.botao-itens')
+const itens = document.querySelector('itens')
+
+function criaLista() {
+    const lista = document.createElement('li')
+    return li
+}
+
